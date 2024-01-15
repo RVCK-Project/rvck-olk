42a5e346ea57ee36e36b95fd3bab0f89670b271a
Andy Chiu <andy.chiu@sifive.com>
2024-01-15 05:59:28 +0000

    riscv: vector: use kmem_cache to manage vector context
    The allocation size of thread.vstate.datap is always riscv_v_vsize. So
    it is possbile to use kmem_cache_* to manage the allocation. This gives
    users more information regarding allocation of vector context via
    /proc/slabinfo. And it potentially reduces the latency of the first-use
    trap because of the allocation caches.
    
    Signed-off-by: Andy Chiu <andy.chiu@sifive.com>
    Tested-by: Björn Töpel <bjorn@rivosinc.com>
    Tested-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Link: https://lore.kernel.org/r/20240115055929.4736-10-andy.chiu@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/kernel/process.c
+++ arch/riscv/kernel/process.c
@@ -159,7 +159,7 @@ void arch_release_task_struct(struct task_struct *tsk)
 {
 	/* Free the vector context of datap. */
 	if (has_vector())
-		kfree(tsk->thread.vstate.datap);
+		riscv_v_thread_free(tsk);
 }
 
 int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src)
@@ -210,3 +210,7 @@ int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 
 EXPORT_SYMBOL_GPL(__fstate_save);
 EXPORT_SYMBOL_GPL(__fstate_restore);
+void __init arch_task_cache_init(void)
+{
+	riscv_v_setup_ctx_cache();
+}
