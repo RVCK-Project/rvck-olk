c798abd1b2314ef4b37c202f7924f58c6c95c247
Mingzheng Xing <xingmingzheng@iscas.ac.cn>
2025-07-06 15:13:33 +0800

    riscv: sg2042: Enable msi-x whitelist only on sg2042
    riscv inclusion
    category: bugfix
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/89
    
    --------------------------------
    
    The current implementation fails the kernel build if you turn off the
    cadence config for PCIe, so turn on the whitelist for the sg2042 platform
    only.
    
    Fixes: fc62a73122e1 ("sg2042: drivers: pcie: Create msi-x whitelist,turn on msi-x for top intr")
    Signed-off-by: Mingzheng Xing <xingmingzheng@iscas.ac.cn>

--- drivers/net/ethernet/intel/i40e/i40e_common.c
+++ drivers/net/ethernet/intel/i40e/i40e_common.c
@@ -3213,8 +3213,11 @@ static void i40e_parse_discover_capabilities(struct i40e_hw *hw, void *buff,
 			p->base_queue = phys_id;
 			break;
 		case I40E_AQ_CAP_ID_MSIX:
-			//p->num_msix_vectors = number;
+#ifndef CONFIG_PCIE_CADENCE_SOPHGO
+			p->num_msix_vectors = number;
+#else
 			p->num_msix_vectors = 8;
+#endif /* CONFIG_PCIE_CADENCE_SOPHGO */
 			i40e_debug(hw, I40E_DEBUG_INIT,
 				   "HW Capability: MSIX vector count = %d\n",
 				   p->num_msix_vectors);

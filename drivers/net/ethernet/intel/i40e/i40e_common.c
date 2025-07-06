RVCK-Project/rvck-olk

commit fc62a73122e129d916fede67c54d8cf68bb5d5a5

sg2042: drivers: pcie: Create msi-x whitelist,turn on msi-x for top intr
community inclusion
category: feature
bugzilla: https://gitee.com/openeuler/riscv-kernel/issues/I9DRVT
Reference: https://github.com/xmzzz/linux-riscv/commit/21778daf59165f08bbd2b90df247edb406b07610

--------------------------------

Turn on msi-x for top intr. Create msi-x whitelist,
Limited the number of msi-x interrupts for inter x520 and wangxun NIC.

Signed-off-by: chengjun.li <chengjun.li@sophgo.com>
[Fixed conflicts with OLK-6.6]
Signed-off-by: Mingzheng Xing <xingmingzheng@iscas.ac.cn>

drivers/net/ethernet/intel/i40e/i40e_common.c
@@ -3213,7 +3213,8 @@ static void i40e_parse_discover_capabilities(struct i40e_hw *hw, void *buff,
 			p->base_queue = phys_id;
 			break;
 		case I40E_AQ_CAP_ID_MSIX:
-			p->num_msix_vectors = number;
+			//p->num_msix_vectors = number;
+			p->num_msix_vectors = 8;
 			i40e_debug(hw, I40E_DEBUG_INIT,
 				   "HW Capability: MSIX vector count = %d\n",
 				   p->num_msix_vectors);

drivers/net/ethernet/intel/ixgbe/ixgbe_type.h
@@ -2069,7 +2069,7 @@ enum {
 #define IXGBE_DEVICE_CAPS       0x2C
 #define IXGBE_SERIAL_NUMBER_MAC_ADDR 0x11
 #define IXGBE_PCIE_MSIX_82599_CAPS  0x72
-#define IXGBE_MAX_MSIX_VECTORS_82599	0x40
+#define IXGBE_MAX_MSIX_VECTORS_82599	0x09
 #define IXGBE_PCIE_MSIX_82598_CAPS  0x62
 #define IXGBE_MAX_MSIX_VECTORS_82598	0x13
 

drivers/pci/msi/msi.c
@@ -12,6 +12,7 @@
 
 #include "../pci.h"
 #include "msi.h"
+#include "../controller/cadence/pcie-cadence-sophgo.h"
 
 #ifdef CONFIG_HISI_VIRTCCA_CODA
 #include <asm/virtcca_cvm_host.h>
@@ -850,66 +851,70 @@ int __pci_enable_msix_range(struct pci_dev *dev, struct msix_entry *entries, int
 	}
 #endif
 
-	if (maxvec < minvec)
-		return -ERANGE;
+	if (check_vendor_id(dev, vendor_id_list, vendor_id_list_num)) {
+		if (maxvec < minvec)
+			return -ERANGE;
 
-	if (dev->msi_enabled) {
-		pci_info(dev, "can't enable MSI-X (MSI already enabled)\n");
-		return -EINVAL;
-	}
-
-	if (WARN_ON_ONCE(dev->msix_enabled))
-		return -EINVAL;
-
-	/* Check MSI-X early on irq domain enabled architectures */
-	if (!pci_msi_domain_supports(dev, MSI_FLAG_PCI_MSIX, ALLOW_LEGACY))
-		return -ENOTSUPP;
-
-	if (!pci_msi_supported(dev, nvec) || dev->current_state != PCI_D0)
-		return -EINVAL;
-
-	hwsize = pci_msix_vec_count(dev);
-	if (hwsize < 0)
-		return hwsize;
-
-	if (!pci_msix_validate_entries(dev, entries, nvec))
-		return -EINVAL;
-
-	if (hwsize < nvec) {
-		/* Keep the IRQ virtual hackery working */
-		if (flags & PCI_IRQ_VIRTUAL)
-			hwsize = nvec;
-		else
-			nvec = hwsize;
-	}
-
-	if (nvec < minvec)
-		return -ENOSPC;
-
-	rc = pci_setup_msi_context(dev);
-	if (rc)
-		return rc;
-
-	if (!pci_setup_msix_device_domain(dev, hwsize))
-		return -ENODEV;
-
-	for (;;) {
-		if (affd) {
-			nvec = irq_calc_affinity_vectors(minvec, nvec, affd);
-			if (nvec < minvec)
-				return -ENOSPC;
+		if (dev->msi_enabled) {
+			pci_info(dev, "can't enable MSI-X (MSI already enabled)\n");
+			return -EINVAL;
 		}
 
-		rc = msix_capability_init(dev, entries, nvec, affd);
-		if (rc == 0)
-			return nvec;
+		if (WARN_ON_ONCE(dev->msix_enabled))
+			return -EINVAL;
 
-		if (rc < 0)
-			return rc;
-		if (rc < minvec)
+		/* Check MSI-X early on irq domain enabled architectures */
+		if (!pci_msi_domain_supports(dev, MSI_FLAG_PCI_MSIX, ALLOW_LEGACY))
+			return -ENOTSUPP;
+
+		if (!pci_msi_supported(dev, nvec) || dev->current_state != PCI_D0)
+			return -EINVAL;
+
+		hwsize = pci_msix_vec_count(dev);
+		if (hwsize < 0)
+			return hwsize;
+
+		if (!pci_msix_validate_entries(dev, entries, nvec))
+			return -EINVAL;
+
+		if (hwsize < nvec) {
+			/* Keep the IRQ virtual hackery working */
+			if (flags & PCI_IRQ_VIRTUAL)
+				hwsize = nvec;
+			else
+				nvec = hwsize;
+		}
+
+		if (nvec < minvec)
 			return -ENOSPC;
 
-		nvec = rc;
+		rc = pci_setup_msi_context(dev);
+		if (rc)
+			return rc;
+
+		if (!pci_setup_msix_device_domain(dev, hwsize))
+			return -ENODEV;
+
+		for (;;) {
+			if (affd) {
+				nvec = irq_calc_affinity_vectors(minvec, nvec, affd);
+				if (nvec < minvec)
+					return -ENOSPC;
+			}
+
+			rc = msix_capability_init(dev, entries, nvec, affd);
+			if (rc == 0)
+				return nvec;
+
+			if (rc < 0)
+				return rc;
+			if (rc < minvec)
+				return -ENOSPC;
+
+			nvec = rc;
+		}
+	} else {
+		return -1;
 	}
 }
 

commit c798abd1b2314ef4b37c202f7924f58c6c95c247

riscv: sg2042: Enable msi-x whitelist only on sg2042
riscv inclusion
category: bugfix
bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/89

--------------------------------

The current implementation fails the kernel build if you turn off the
cadence config for PCIe, so turn on the whitelist for the sg2042 platform
only.

Fixes: fc62a73122e1 ("sg2042: drivers: pcie: Create msi-x whitelist,turn on msi-x for top intr")
Signed-off-by: Mingzheng Xing <xingmingzheng@iscas.ac.cn>

drivers/net/ethernet/intel/i40e/i40e_common.c
@@ -3213,8 +3213,11 @@ static void i40e_parse_discover_capabilities(struct i40e_hw *hw, void *buff,
 			p->base_queue = phys_id;
 			break;
 		case I40E_AQ_CAP_ID_MSIX:
-			//p->num_msix_vectors = number;
+#ifndef CONFIG_PCIE_CADENCE_SOPHGO
+			p->num_msix_vectors = number;
+#else
 			p->num_msix_vectors = 8;
+#endif /* CONFIG_PCIE_CADENCE_SOPHGO */
 			i40e_debug(hw, I40E_DEBUG_INIT,
 				   "HW Capability: MSIX vector count = %d\n",
 				   p->num_msix_vectors);

drivers/net/ethernet/intel/ixgbe/ixgbe_type.h
@@ -2069,7 +2069,13 @@ enum {
 #define IXGBE_DEVICE_CAPS       0x2C
 #define IXGBE_SERIAL_NUMBER_MAC_ADDR 0x11
 #define IXGBE_PCIE_MSIX_82599_CAPS  0x72
+
+#ifndef CONFIG_PCIE_CADENCE_SOPHGO
+#define IXGBE_MAX_MSIX_VECTORS_82599   0x40
+#else
 #define IXGBE_MAX_MSIX_VECTORS_82599	0x09
+#endif /* CONFIG_PCIE_CADENCE_SOPHGO */
+
 #define IXGBE_PCIE_MSIX_82598_CAPS  0x62
 #define IXGBE_MAX_MSIX_VECTORS_82598	0x13
 

drivers/pci/msi/msi.c
@@ -12,7 +12,10 @@
 
 #include "../pci.h"
 #include "msi.h"
+
+#ifdef CONFIG_PCIE_CADENCE_SOPHGO
 #include "../controller/cadence/pcie-cadence-sophgo.h"
+#endif /* CONFIG_PCIE_CADENCE_SOPHGO */
 
 #ifdef CONFIG_HISI_VIRTCCA_CODA
 #include <asm/virtcca_cvm_host.h>
@@ -851,70 +854,71 @@ int __pci_enable_msix_range(struct pci_dev *dev, struct msix_entry *entries, int
 	}
 #endif
 
-	if (check_vendor_id(dev, vendor_id_list, vendor_id_list_num)) {
-		if (maxvec < minvec)
-			return -ERANGE;
+#ifdef CONFIG_PCIE_CADENCE_SOPHGO
+	if (!check_vendor_id(dev, vendor_id_list, vendor_id_list_num))
+		return -EINVAL;
+#endif /* CONFIG_PCIE_CADENCE_SOPHGO */
 
-		if (dev->msi_enabled) {
-			pci_info(dev, "can't enable MSI-X (MSI already enabled)\n");
-			return -EINVAL;
+	if (maxvec < minvec)
+		return -ERANGE;
+
+	if (dev->msi_enabled) {
+		pci_info(dev, "can't enable MSI-X (MSI already enabled)\n");
+		return -EINVAL;
+	}
+
+	if (WARN_ON_ONCE(dev->msix_enabled))
+		return -EINVAL;
+
+	/* Check MSI-X early on irq domain enabled architectures */
+	if (!pci_msi_domain_supports(dev, MSI_FLAG_PCI_MSIX, ALLOW_LEGACY))
+		return -ENOTSUPP;
+
+	if (!pci_msi_supported(dev, nvec) || dev->current_state != PCI_D0)
+		return -EINVAL;
+
+	hwsize = pci_msix_vec_count(dev);
+	if (hwsize < 0)
+		return hwsize;
+
+	if (!pci_msix_validate_entries(dev, entries, nvec))
+		return -EINVAL;
+
+	if (hwsize < nvec) {
+		/* Keep the IRQ virtual hackery working */
+		if (flags & PCI_IRQ_VIRTUAL)
+			hwsize = nvec;
+		else
+			nvec = hwsize;
+	}
+
+	if (nvec < minvec)
+		return -ENOSPC;
+
+	rc = pci_setup_msi_context(dev);
+	if (rc)
+		return rc;
+
+	if (!pci_setup_msix_device_domain(dev, hwsize))
+		return -ENODEV;
+
+	for (;;) {
+		if (affd) {
+			nvec = irq_calc_affinity_vectors(minvec, nvec, affd);
+			if (nvec < minvec)
+				return -ENOSPC;
 		}
 
-		if (WARN_ON_ONCE(dev->msix_enabled))
-			return -EINVAL;
+		rc = msix_capability_init(dev, entries, nvec, affd);
+		if (rc == 0)
+			return nvec;
 
-		/* Check MSI-X early on irq domain enabled architectures */
-		if (!pci_msi_domain_supports(dev, MSI_FLAG_PCI_MSIX, ALLOW_LEGACY))
-			return -ENOTSUPP;
-
-		if (!pci_msi_supported(dev, nvec) || dev->current_state != PCI_D0)
-			return -EINVAL;
-
-		hwsize = pci_msix_vec_count(dev);
-		if (hwsize < 0)
-			return hwsize;
-
-		if (!pci_msix_validate_entries(dev, entries, nvec))
-			return -EINVAL;
-
-		if (hwsize < nvec) {
-			/* Keep the IRQ virtual hackery working */
-			if (flags & PCI_IRQ_VIRTUAL)
-				hwsize = nvec;
-			else
-				nvec = hwsize;
-		}
-
-		if (nvec < minvec)
+		if (rc < 0)
+			return rc;
+		if (rc < minvec)
 			return -ENOSPC;
 
-		rc = pci_setup_msi_context(dev);
-		if (rc)
-			return rc;
-
-		if (!pci_setup_msix_device_domain(dev, hwsize))
-			return -ENODEV;
-
-		for (;;) {
-			if (affd) {
-				nvec = irq_calc_affinity_vectors(minvec, nvec, affd);
-				if (nvec < minvec)
-					return -ENOSPC;
-			}
-
-			rc = msix_capability_init(dev, entries, nvec, affd);
-			if (rc == 0)
-				return nvec;
-
-			if (rc < 0)
-				return rc;
-			if (rc < minvec)
-				return -ENOSPC;
-
-			nvec = rc;
-		}
-	} else {
-		return -1;
+		nvec = rc;
 	}
 }
 