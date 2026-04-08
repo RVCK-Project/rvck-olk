RVCK-Project/rvck-olk

commit e46e8e85417f76f425857da1f5037a3ccb111136

sched/fair: scale wake_wide() threshold by SMT width
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8882

--------------------------------

wake_wide() uses sd_llc_size as the spreading threshold to detect wide
waker/wakee relationships and to disable wake_affine() for those cases.

On SMT systems, sd_llc_size counts logical CPUs rather than physical
cores. This inflates the wake_wide() threshold, allowing wake_affine()
to pack more tasks into one LLC domain than the actual compute capacity
of its physical cores can sustain. The resulting SMT interference may
cost more than the cache-locality benefit wake_affine() intends to gain.

Scale the factor by the SMT width of the current CPU so that it
approximates the number of independent physical cores in the LLC domain,
making wake_wide() more likely to kick in before SMT interference
becomes significant. On non-SMT systems the SMT width is 1 and behaviour
is unchanged.

Fixes: 63b0e9edceec ("sched/fair: Beef up wake_wide()")
Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>

kernel/sched/fair.c
@@ -7850,6 +7850,14 @@ static int wake_wide(struct task_struct *p)
 	unsigned int slave = p->wakee_flips;
 	int factor = __this_cpu_read(sd_llc_size);
 
+	/* Scale factor to physical-core count to account for SMT interference. */
+	if (sched_feat(WA_SMT)) {
+		int smt_width = cpumask_weight(cpu_smt_mask(smp_processor_id()));
+
+		if (smt_width > 1)
+			factor = DIV_ROUND_UP(factor, smt_width);
+	}
+
 	if (master < slave)
 		swap(master, slave);
 	if (slave < factor || master < slave * factor)

kernel/sched/features.h
@@ -128,3 +128,5 @@ SCHED_FEAT(SOFT_DOMAIN, false)
 #ifdef CONFIG_SCHED_SOFT_QUOTA
 SCHED_FEAT(SOFT_QUOTA, false)
 #endif
+
+SCHED_FEAT(WA_SMT, false)

commit 689df2df5149bad16a700bc9d6c67c4d53496cf7

sched/fair: Fix compile error without CONFIG_SCHED_SMT
community inclusion
category: bugfix

--------------------------------

The WA_SMT feat used depends on target system, conditional
compilation should be used.

Fixes: e46e8e85417f ("sched/fair: scale wake_wide() threshold by SMT width")
Signed-off-by: WeiHao Li <cn.liweihao@gmail.com>

kernel/sched/fair.c
@@ -7850,6 +7850,7 @@ static int wake_wide(struct task_struct *p)
 	unsigned int slave = p->wakee_flips;
 	int factor = __this_cpu_read(sd_llc_size);
 
+#if defined(CONFIG_SCHED_SMT)
 	/* Scale factor to physical-core count to account for SMT interference. */
 	if (sched_feat(WA_SMT)) {
 		int smt_width = cpumask_weight(cpu_smt_mask(smp_processor_id()));
@@ -7857,6 +7858,7 @@ static int wake_wide(struct task_struct *p)
 		if (smt_width > 1)
 			factor = DIV_ROUND_UP(factor, smt_width);
 	}
+#endif
 
 	if (master < slave)
 		swap(master, slave);

kernel/sched/features.h
@@ -129,4 +129,6 @@ SCHED_FEAT(SOFT_DOMAIN, false)
 SCHED_FEAT(SOFT_QUOTA, false)
 #endif
 
+#if defined(CONFIG_SCHED_SMT)
 SCHED_FEAT(WA_SMT, false)
+#endif